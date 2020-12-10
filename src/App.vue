285bc2f3e609147e86f110a0aa949feab932afaa
ilic93 <markoilic.va93@gmail.com>
2020-12-10 22:22:07 +0100

    static page

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-container>
+      <h1 class="header">Final Countdown</h1>
+      <b-nav pills>
+        <b-nav-item disabled>Take 5</b-nav-item>
+        <b-nav-item disabled>10</b-nav-item>
+        <b-nav-item disabled>20</b-nav-item>
+        <b-nav-item disabled>30</b-nav-item>
+        <b-nav-item disabled>60</b-nav-item>
+        <b-nav-form @submit.prevent="alert('Form Submitted')">
+          <b-form-input class="mr-1" placeholder="How many minutes?"></b-form-input>
+          <b-button type="submit">Ok</b-button>
+        </b-nav-form>
+      </b-nav>
+      <div class="timeLeft">10:00</div>
+      <div class="endTime">Be right back!</div>
+    </b-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
@@ -23,6 +33,39 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  padding-top: 60px;
+  min-height: 100vh;
+  background: linear-gradient(lightblue, red);
+}
+
+.header {
+  color: black;
+  text-shadow: 2px 2px 0 rgba(0, 0, 0, 0.5);
+  margin-bottom: 30px;
+}
+
+.nav-item {
+  flex: 1;
+  color: black;
+  border-right: 1px dotted black;
+}
+
+.nav-item > a {color: black !important;}
+
+.nav-item:hover {
+  cursor: pointer;
+  background-color: hsla(0, 100%, 50%, 0.5);
+  font-weight: bold;
+  border-radius: 15px;
+  box-shadow: 0 2px 2px black;
+}
+
+.timeLeft {
+  font-size: 150px;
+  color: black;
+}
+.endTime {
+  font-size: 2rem;
+  color: black;
 }
 </style>
