1c8996cbbf57ccd0fb1f51814b3b3611136fc2fb
ilic93 <markoilic.va93@gmail.com>
2020-12-11 13:53:30 +0100

    added methods for countdown

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,22 @@
 <template>
   <div id="app">
     <b-container>
-      <h1 class="header">Final Countdown</h1>
-      <b-nav pills>
-        <b-nav-item disabled>Take 5</b-nav-item>
-        <b-nav-item disabled>10</b-nav-item>
-        <b-nav-item disabled>20</b-nav-item>
-        <b-nav-item disabled>30</b-nav-item>
-        <b-nav-item disabled>60</b-nav-item>
-        <b-nav-form @submit.prevent="alert('Form Submitted')">
-          <b-form-input class="mr-1" placeholder="How many minutes?"></b-form-input>
-          <b-button type="submit">Ok</b-button>
-        </b-nav-form>
-      </b-nav>
-      <div class="timeLeft">10:00</div>
-      <div class="endTime">Be right back!</div>
+      <div class="main-content">
+        <h1 class="header">Final Countdown</h1>
+        <b-nav pills>
+          <b-nav-item @click="timer(300)">Take 5</b-nav-item>
+          <b-nav-item @click="timer(600)">10</b-nav-item>
+          <b-nav-item @click="timer(1200)">20</b-nav-item>
+          <b-nav-item @click="timer(1800)">30</b-nav-item>
+          <b-nav-item @click="timer(3600)">60</b-nav-item>
+          <b-nav-form @submit.prevent="customTimer()">
+            <b-form-input class="mr-1" v-model="minutes" placeholder="How many minutes?"></b-form-input>
+            <b-button type="submit">Ok</b-button>
+          </b-nav-form>
+        </b-nav>
+        <div class="timeLeft">{{ timeLeft || 'Take a Break :)' }}</div>
+        <div class="endTime">{{ endTime }}</div>
+      </div>
     </b-container>
   </div>
 </template>
@@ -22,7 +24,53 @@
 <script>
 
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      timeLeft: '',
+      endTime: '',
+      countdown: null,
+      minutes: null
+    }
+  },
+  methods: {
+    getTimeLeft(seconds) {
+      let secondsLeft = seconds
+      const minutes = Math.floor(secondsLeft / 60)
+      secondsLeft %= 60
+      this.timeLeft = `${minutes < 10 ? '0' : ''}${minutes}:${secondsLeft < 10 ? '0' : ''}${secondsLeft}`
+    },
+    getEndTime(then) {
+      const x = new Date(then)
+      const hours = x.getHours()
+      const minutes = x.getMinutes()
+      this.endTime = `Be right back at ${hours < 10 ? '0' : ''}${hours}:${minutes < 10 ? '0' : ''}${minutes}!`
+    },
+    timer(seconds) {
+      clearInterval(this.countdown)
+
+      const now = Date.now()
+      const then = now + seconds * 1000
+      this.getTimeLeft(seconds)
+      this.getEndTime(then)
+
+      this.countdown = setInterval(() => {
+        const newNow = Math.round((then - Date.now()) / 1000)
+        if(newNow < 0) {
+          console.log('THE END')
+          clearInterval(this.countdown)
+        } else {
+          this.getTimeLeft(newNow)
+        }
+      }, 1000)
+    },
+    customTimer() {
+      console.log(this.minutes)
+      const x = parseFloat(this.minutes)
+      this.timer(x * 60)
+      this.minutes = null
+    }
+  }
 }
 </script>
 
@@ -38,10 +86,18 @@ export default {
   background: linear-gradient(lightblue, red);
 }
 
+.main-content {
+  border: 3px solid blue;
+  border-radius: 5px;
+  background: rgba(255, 255, 255, 50%);
+  box-shadow: 5px 5px 2px rgba(0, 0, 0, 0.5);
+}
+
 .header {
   color: black;
   text-shadow: 2px 2px 0 rgba(0, 0, 0, 0.5);
   margin-bottom: 30px;
+  border-bottom: 1px solid grey;
 }
 
 .nav-item {
@@ -63,6 +119,7 @@ export default {
 .timeLeft {
   font-size: 150px;
   color: black;
+  transition: all 2s;
 }
 .endTime {
   font-size: 2rem;
